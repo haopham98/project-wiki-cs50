546becd3c4701a9f25a4e14045d91f6fdfba01e1
haopham6798 <phamhoanghao@vnpt.vn>
2025-06-10 11:20:41 +0700

    feat(encyclopeida): add search feature

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>/", views.view_entry, name="view"),
+    path("search/", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,8 +9,36 @@ def index(request):
         "entries": util.list_entries()
  })
 
+def view_entry(request, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        return HttpResponse("Title not found.", status=404)
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": entry
+    })
 
-    
+
+def search(request):
+    query = request.GET.get('q', '')
+    entry = util.get_entry(query)
+    if entry:
+        return render(request, "encyclopedia/entry.html", {
+            "title": query,
+            "content": entry
+        })
+    else:
+        entries = util.list_entries()
+        matching_entries = [entry for entry in entries if query.lower() in entry.lower()]
+        
+        if not matching_entries:        
+            return HttpResponse("No entry found for the given query.", status=404)
+        
+        return render(request, "encyclopedia/index.html", {
+            "entries": matching_entries 
+        })
+       
+
 
     
 
